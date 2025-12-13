c7113957bc01d9e2df75774d7cb5f582aedec8fa
yashselokar2025 <yash20selokar22@gmail.com>
2025-12-13 23:44:33 +0530

    Add files via upload

---
+++ ASCII.cpp
@@ -0,0 +1,12 @@
+//ASCII
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    
+    printf("The ASCII value of %c is %d\n", ch, ch);
+
+    return 0;
+}
+
